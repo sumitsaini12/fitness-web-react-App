6d83fddb0734372657f6f6e19b7a9293263c368f
sumit saini <sumitsaini95488@gamil.com>
2023-08-17 20:04:41 +0530

    initial Fitness App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-red-500 font-semibold text-5xl text-center">
+        This is Fitness App
+      </p>
+    </>
+  );
+}
+
+export default App;
